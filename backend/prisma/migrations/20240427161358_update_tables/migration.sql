261c464bff3d125e2abf70bf44152ad11e900bc3
Alvaro Henrique <alvaroindaial15@gmail.com>
2024-04-27 13:19:35 -0300

    feat: criando uma seed para user

---
+++ backend/prisma/migrations/20240427161358_update_tables/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `news` MODIFY `image` VARCHAR(191) NULL;
+
+-- AlterTable
+ALTER TABLE `users` MODIFY `avatar` VARCHAR(191) NULL;
---
+++ backend/src/prisma/seed.ts
@@ -0,0 +1,36 @@
+import { hashSync } from 'bcryptjs';
+import { Prisma } from '.';
+
+async function main() {
+    const role1 = await Prisma.role.create({
+        data: { type: 'admin' }
+    })
+    const role2 = await Prisma.role.create({
+        data: { type: 'maintenance' }
+    })
+    const role3 = await Prisma.role.create({
+        data: { type: 'user' }
+    })
+
+    const user1 = await Prisma.user.create({
+        data: {
+            name: 'Alvaro',
+            lastname: 'Henrique',
+            email: 'alvaro@gmail.com',
+            phone: '(47) 9 9999-9999',
+            password: hashSync('123', 8),
+            avatar: '',
+            roleId: 1
+        }
+    })
+ 
+}
+
+main()
+    .then(async () => {
+        await Prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await Prisma.$disconnect()
+    })
