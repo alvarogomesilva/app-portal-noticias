a7b89411f0024459995c92f79513a7e08fcd8e43
Alvaro Henrique <alvaroindaial15@gmail.com>
2024-11-03 11:00:37 -0300

    feat: adiciona modal e exlui noticia

--- frontend/src/pages/ActionPage/index.tsx
+++ frontend/src/pages/ActionPage/index.tsx
@@ -10,12 +10,12 @@ export default function ActionPage() {
     return (
         <>
             <Navbar />
-            <div className="flex flex-wrap lg:w-2/5 sm:mx-auto sm:mb-2  p-4">
+            <div className="flex flex-wrap lg:w-2/5 sm:mx-auto sm:mb-2 p-4">
                 {roleId === 1 && (
                     <>
                         <h2 className="text-3xl my-2 text-indigo-600">➥ Usuários</h2>
                         <div className="p-2 w-full">
-                            <Link to="/new-user" className="bg-gray-200 rounded flex p-4 h-full items-center">
+                            <Link to="/new-user" className="bg-gray-50 rounded flex p-4 h-full items-center">
                                 <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3"
                                     className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                                     <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
@@ -25,7 +25,7 @@ export default function ActionPage() {
                             </Link>
                         </div>
                         <div className="p-2  w-full">
-                            <Link to="/list-user" className="bg-gray-200 rounded flex p-4 h-full items-center">
+                            <Link to="/list-user" className="bg-gray-50 rounded flex p-4 h-full items-center">
                                 <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3"
                                     className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                                     <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
@@ -40,7 +40,7 @@ export default function ActionPage() {
 
                 <h2 className="text-3xl mt-4 text-indigo-600">➥ Noticias</h2>
                 <div className="p-2 w-full">
-                    <Link to="/new-notice" className="bg-gray-200 rounded flex p-4 h-full items-center">
+                    <Link to="/new-notice" className="bg-gray-50 rounded flex p-4 h-full items-center">
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3"
                             className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                             <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
@@ -50,7 +50,7 @@ export default function ActionPage() {
                     </Link>
                 </div>
                 <div className="p-2  w-full">
-                    <Link to="/list-notice" className="bg-gray-200 rounded flex p-4 h-full items-center">
+                    <Link to="/list-notice" className="bg-gray-50 rounded flex p-4 h-full items-center">
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3"
                             className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                             <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
--- frontend/src/pages/ListNoticesPage/index.tsx
+++ frontend/src/pages/ListNoticesPage/index.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import { api, BASE_URL } from "../../api";
-import { Navbar } from "../../components/Navbar"
+import { Navbar } from "../../components/Navbar";
 import { Link } from "react-router-dom";
 import { getToken } from "../../utils/getToken";
+import { Dialog } from "../../components/Dialog";
 
 interface Notice {
     id: string;
@@ -16,19 +17,20 @@ interface Notice {
 }
 
 export function ListNoticesPage() {
-    const [notices, setNotices] = useState<Notice[]>([])
+    const [notices, setNotices] = useState<Notice[]>([]);
+    const [isShowDialog, setIsShowDialog] = useState(false);
+    const [selectedNoticeId, setSelectedNoticeId] = useState<string | null>(null);
 
-    // Carrega todas as notices
     useEffect(() => {
         async function fetchNews() {
-            const token = getToken()
+            const token = getToken();
             try {
                 const request = await api.get('/news', {
                     headers: {
-                        Authorization: `Bearer ${token}`
-                    }
+                        Authorization: `Bearer ${token}`,
+                    },
                 });
-                setNotices(request.data)
+                setNotices(request.data);
             } catch (error) {
                 console.log("Erro ao carregar notícias:", error);
             }
@@ -37,43 +39,64 @@ export function ListNoticesPage() {
         fetchNews();
     }, []);
 
+    const handleDeleteClick = (id: string) => {
+        setSelectedNoticeId(id);
+        setIsShowDialog(true);
+    };
+
+    const handleDialogClose = () => {
+        setIsShowDialog(false);
+        setSelectedNoticeId(null);
+    };
+
     return (
         <>
+            {isShowDialog && (
+                <Dialog 
+                    isOpen={isShowDialog}
+                    onClose={handleDialogClose}
+                    noticeId={selectedNoticeId}
+                />
+            )}
             <Navbar />
             <div className="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 gap-4 max-w-5xl mx-auto mt-10 px-4">
                 {notices.length > 0 ? (
                     notices.map((notice) => (
-
                         <div
                             key={notice.id}
-                            className="flex flex-col justify-between max-w-sm mx-auto bg-white rounded-lg overflow-hidden shadow-lg">
-
-                            <img className="w-full h-56 object-cover" src={`${BASE_URL}/files/banners/${notice.image}`} alt="Imagem do Card" />
-
-
+                            className="flex flex-col justify-between max-w-sm mx-auto bg-white rounded-lg overflow-hidden shadow-lg"
+                        >
+                            <img 
+                                className="w-full h-56 object-cover" 
+                                src={`${BASE_URL}/files/banners/${notice.image}`} 
+                                alt="Imagem do Card" 
+                            />
                             <div className="p-4">
                                 <h2 className="text-xl font-bold text-gray-800">{notice.title}</h2>
                                 <p className="text-gray-500 text-sm mt-2">{notice.description}</p>
                             </div>
-
                             <div className="p-4">
-
-                                <div className="flex mt-4 md:mt-6">
-                                    <Link to={`/edit-notice/${notice.id}`} className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700" >Atualizar</Link>
-
+                                <div className="flex justify-around mt-4 md:mt-6">
+                                    <Link 
+                                        to={`/edit-notice/${notice.id}`} 
+                                        className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700"
+                                    >
+                                        Atualizar
+                                    </Link>
                                     <button
-
-                                        className="py-2 px-4 ms-2 text-sm font-medium text-white focus:outline-none bg-red-500 rounded-lg border border-gray-200 hover:bg-red-700  focus:z-10 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Deletar</button>
+                                        onClick={() => handleDeleteClick(notice.id)}
+                                        className="py-2 px-4 ms-2 text-sm font-medium text-white focus:outline-none bg-red-500 rounded-lg border border-gray-200 hover:bg-red-700 focus:z-10 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                                    >
+                                        Deletar
+                                    </button>
                                 </div>
                             </div>
                         </div>
-
                     ))
                 ) : (
                     <h1>Nenhuma notícia cadastrada!</h1>
                 )}
-
             </div>
         </>
-    )
+    );
 }
